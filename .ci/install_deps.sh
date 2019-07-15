20ecbb331d103ac148eff8eaa0a9ef107f588dee
Aaron Petkau <aaron.petkau@canada.ca>
2019-07-15 15:03:32 -0500

    Update chromedriver version

--- .ci/install_deps.sh
+++ .ci/install_deps.sh
@@ -2,7 +2,7 @@
 sudo pip install -U bioblend pytest pytest-cov pytest-mock requests==2.6 requests-oauthlib==0.4.2 subprocess32 selenium
 
 # Install chromedriver
-CHROME_DRIVER_VERSION=2.28
+CHROME_DRIVER_VERSION=75.0.3770.140
 wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 sudo rm -rf /opt/selenium/chromedriver
 sudo unzip /tmp/chromedriver_linux64.zip -d /opt/selenium
