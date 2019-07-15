7350f7e7f3e48cc96447a917da165597cd24c2fa
Aaron Petkau <aaron.petkau@canada.ca>
2019-07-15 12:15:22 -0500

    sudo some commands

--- .ci/install_deps.sh
+++ .ci/install_deps.sh
@@ -4,12 +4,12 @@ sudo pip install -U bioblend pytest pytest-cov pytest-mock requests==2.6 request
 # Install chromedriver
 CHROME_DRIVER_VERSION=2.28
 wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-rm -rf /opt/selenium/chromedriver
-unzip /tmp/chromedriver_linux64.zip -d /opt/selenium
+sudo rm -rf /opt/selenium/chromedriver
+sudo unzip /tmp/chromedriver_linux64.zip -d /opt/selenium
 rm /tmp/chromedriver_linux64.zip
-mv /opt/selenium/chromedriver /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION
-chmod 755 /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION
-ln -fs /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION /usr/bin/chromedriver
+sudo mv /opt/selenium/chromedriver /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION
+sudo chmod 755 /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION
+sudo ln -fs /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION /usr/bin/chromedriver
 
 echo '{ "allow_root": true }' > /root/.bowerrc
 sed -i -e 's/localhost:3306/mysql:3306/g' irida_import/tests/integration/test_irida_import_int.py
