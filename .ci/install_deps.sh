440925eff9058a3060ed043af7bde114c624970b
Aaron Petkau <aaron.petkau@canada.ca>
2019-07-15 19:11:12 -0500

    Removed changing IRIDA mysql info

--- .ci/install_deps.sh
+++ .ci/install_deps.sh
@@ -12,8 +12,3 @@ sudo chmod 755 /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION
 sudo ln -fs /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION /usr/bin/chromedriver
 
 sudo echo '{ "allow_root": true }' > /root/.bowerrc
-sed -i -e 's/localhost:3306/mysql:3306/g' irida_import/tests/integration/test_irida_import_int.py
-sed -i -e 's/password=test/password=password/g' irida_import/tests/integration/test_irida_import_int.py
-sed -i -e 's/mysql -u test -ptest/mysql -h mysql -u test -ppassword/g' irida_import/tests/integration/test_irida_import_int.py
-
-sed -i -e 's/test:test@localhost/test:password@mysql/g' irida_import/tests/integration/bash_scripts/install.sh
