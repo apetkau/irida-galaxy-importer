7faa87b2c6b803fab6861892296e316ce27f333e
Aaron Petkau <aaron.petkau@canada.ca>
2019-07-19 10:27:21 -0500

    Added script for running tests locally

--- .ci/install_deps.sh
+++ .ci/install_deps.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -e
-virtualenv venv
-source venv/bin/activate
+
+CHROMEDRIVER_VERSION=$1
+
+python2 -m virtualenv .venv
+source .venv/bin/activate
 pip install -U bioblend pytest pytest-cov pytest-mock requests==2.6 requests-oauthlib==1.2.0 subprocess32 selenium
 
-# Install chromedriver
-CHROME_DRIVER_VERSION=75.0.3770.140
-echo "Downloading Chromedriver Version: $CHROME_DRIVER_VERSION"
-wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-unzip /tmp/chromedriver_linux64.zip -d venv/bin
-chmod 755 venv/bin/chromedriver
+# Install chromedriver if not correct version
+TEST_CHROMEDRIVER_VERSION=`chromedriver --version | sed -e 's/^ChromeDriver //' -e 's/ (.*//' 2>/dev/null`
+if [ "$TEST_CHROMEDRIVER_VERSION" != "$CHROMEDRIVER_VERSION" ];
+then
+	echo "Downloading Chromedriver Version: $CHROME_DRIVER_VERSION"
+	wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+	unzip /tmp/chromedriver_linux64.zip -d .venv/bin
+	chmod 755 .venv/bin/chromedriver
+else
+	echo "Chromedriver version [" $CHROMEDRIVER_VERSION "] already exists, not installing"
+fi
